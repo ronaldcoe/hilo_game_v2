821227639e2491ec4750053678083ff502b56858
ronaldcoe <ronaldcoello85@gmail.com>
2021-11-04 21:54:41 -0600

    Called the game instructions fixed errors

--- hilo.py
+++ hilo.py
@@ -9,6 +9,13 @@ class Hilo_Game:
         self.cont_validation = False
         self.user_input = ""
 
+    def show_instructions(self):
+        print(100*"-")
+        print("Welcome to the Hilo Game")
+        print("\nInstructions")
+        print("You will start the game with 300 points. The dealer will show you the current card.\nYou will need to guess if the next one will be higher (h) or lower (l).\nThe dealer will show you the next card. You will earn 100 points if you guessed correctly, but you will\nlose 75 points if you guessed incorrectly. If you lose 0 all of your points, the game is over.\nOtherwise the game will continue until you decide to end it by entering (e) at any point in the game.\nGood Luck!")
+        print(100*'-')    
+
     def game_loop(self):
         while not self.exit:
             self.get_input()
@@ -39,21 +46,27 @@ class Hilo_Game:
         old_value = self.deck.get_value(self.top_card)
         self.top_card = self.deck.my_deck.pop(0)
         new_value = self.deck.get_value(self.top_card)
-        if new_value > old_value:
-            if self.user_input == "h":
-                print(f"The next card was {self.top_card}! You win 100 points.\n You have {self.points} points.")
-                self.points += 100
+        if self.user_input != 'e':
+            if new_value > old_value:
+                if self.user_input == "h":
+                    self.points += 100
+                    print(f"The next card was {self.top_card}! You win 100 points.\nYou have {self.points} points.")
+                    
+                else:
+                    self.points -= 75
+                    print(f"The next card was {self.top_card}. You lose 75 points.\nYou have {self.points} points.")
+                    
+            elif new_value < old_value:
+                if self.user_input == "l":
+                    self.points += 100
+                    print(f"The next card was {self.top_card}! You win 100 points.\n You have {self.points} points.")
+                    
+                else:
+                    self.points -= 75
+                    print(f"The next card was {self.top_card}. You lose 75 points.\n You have {self.points} points.")
+                    
             else:
-                print(f"The next card was {self.top_card}. You lose 75 points.\n You have {self.points} points.")
                 self.points -= 75
-        elif new_value < old_value:
-            if self.user_input == "l":
-                print(f"The next card was {self.top_card}! You win 100 points.\n You have {self.points} points.")
-                self.points += 100
-            else:
                 print(f"The next card was {self.top_card}. You lose 75 points.\n You have {self.points} points.")
-                self.points -= 75
-        else:
-            print(f"The next card was {self.top_card}. You lose 75 points.\n You have {self.points} points.")
-            self.points -= 75
+            
 
